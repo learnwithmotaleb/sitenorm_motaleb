35d1fd78c052d1166d618201db2e0fc9fc4aac91
jubayed004 <mohammadjubayed.islam97@gmail.com>
2026-05-16 08:47:04 +0600

    amni

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,10 +7,13 @@ import java.io.FileInputStream
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 
 android {
     namespace = "com.agroclima.sitenorm"
